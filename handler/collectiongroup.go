e09d3d48c0f69f5892c0c38fd13e22cd1fb9585a
riefaldy.kusumo <riefaldy.kusumo@smartfinance.co.id>
2021-11-25 17:43:33 +0700

    Add Handler Collection Group

---
+++ handler/collectiongroup.go
@@ -0,0 +1,64 @@
+package handler
+
+import (
+	"collar-service-api/collectiongroup"
+	"collar-service-api/helper"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type collectiongroupHandler struct {
+	collectiongroupService collectiongroup.Service
+}
+
+func NewCollectionGroupHandler(collectiongroupService collectiongroup.Service) *collectiongroupHandler {
+	return &collectiongroupHandler{collectiongroupService}
+}
+func (h *collectiongroupHandler) ListOfPrivelege(c *gin.Context) {
+	listPrivelge, err := h.collectiongroupService.ListPrivelege()
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("No Data Found", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	response := helper.APIResponse("Data Found", http.StatusOK, "Data Found", listPrivelge)
+
+	c.JSON(http.StatusOK, response)
+}
+func (h *collectiongroupHandler) CollectionGroupNew(c *gin.Context) {
+
+	var input collectiongroup.CreateCGInput
+	// var input collectiongroup.CollectionGroup
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed Created Collection Group", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+	newCG, err := h.collectiongroupService.CreateCG(input)
+	// newCG, err := h.collectiongroupService.CreateCG()
+
+	if err != nil {
+		response := helper.APIResponse("Failed Created Collection Group", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := collectiongroup.FormatCreateCG(newCG)
+
+	response := helper.APIResponse("Collection Group Created", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+
+}
